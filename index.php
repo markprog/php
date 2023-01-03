65d62bfed39c888a9a025d576b006f5b6791beda
Vasilii Sukhorukov <markprog@mail.ru>
2023-01-03 17:09:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <?php
+    {
+        echo "hi". "<br>";
+        echo date(DATE_RSS). "<br>";
+        echo $_SERVER['HTTP_USER_AGENT'];
+    }
+
+    
+
+    ?>
+    
 
 
-echo "hi";
-
-?>
+</head>
+<body>
+    
+</body>
+</html>
