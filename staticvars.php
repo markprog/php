932c0dea1782c27287be746302ebab7b93614c94
Vasilii Sukhorukov <markprog@mail.ru>
2023-01-15 13:14:16 +0300

    Basic PHP Lesson 7

---
+++ staticvars.php
@@ -0,0 +1,17 @@
+<?php
+//Классы и объекты
+class SomePeople {
+    public $age;    //public - зона видимости
+    public $name;
+
+    public static $people = 1;
+}
+
+//echo SomePeople::$people;
+
+$fr = $sc = 3;
+echo $fr . '<br>';
+echo $sc; 
+
+
+?>
